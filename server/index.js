c14ae831740a9c7e572defbb34fe7c38729140fc
Francis Manalang <francismanalang7@gmail.com>
2022-09-14 01:01:32 +0000

    User can start workout feature completed

--- server/index.js
+++ server/index.js
@@ -1,14 +1,36 @@
 require('dotenv/config');
+const pg = require('pg');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
 app.use(staticMiddleware);
 
-app.get('/api/hello', (req, res) => {
-  res.json({ hello: 'world' });
+app.post('/workouts/start', (req, res, next) => {
+  const createdAt = new Date();
+  const isCompleted = false;
+  const exercises = '[]';
+  const sql = `
+  INSERT INTO "workouts" ("createdAt", "isCompleted", "exercises")
+  VALUES ($1, $2, $3)
+  RETURNING *
+  `;
+  const params = [createdAt, isCompleted, exercises];
+  db.query(sql, params)
+    .then(result => {
+      const [newWorkout] = result.rows;
+      res.status(201).json(newWorkout);
+    })
+    .catch(err => next(err));
 });
 
 app.use(errorMiddleware);
